7f7c1901927626ce94bfb79cadba210f46cbd6b0
Nirzhor69 <92746573+Nirzhor69@users.noreply.github.com>
2024-06-05 01:41:49 +0600

    Update nagad.py

--- nagad.py
+++ nagad.py
@@ -7,7 +7,7 @@ headers = {
     "X-KM-User-AspId": "100012345612345",
     "X-KM-User-Agent": "ANDROID/1152",
     "X-KM-DEVICE-FGP": "19DC58E052A91F5B2EB59399AABB2B898CA68CFE780878C0DB69EAAB0553C3C6",
-    "X-KM-Accept-language": "bn",
+    "X-KM-Accept-language": "en",
     "X-KM-AppCode": "01",
 }
 
